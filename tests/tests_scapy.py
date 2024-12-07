f57874b4f56404dfe5a2a939d804406cdbc8280c
Amir Eskandari <am.eskandari@icloud.com>
2024-12-07 09:56:26 -0800

    Create tests_scapy.py

---
+++ tests/tests_scapy.py
@@ -0,0 +1,10 @@
+from scapy.all import IP, TCP, sr1
+
+target_ip = "127.0.0.1"
+packet = IP(dst=target_ip) / TCP(dport=80, flags="S")
+response = sr1(packet, timeout=1)
+
+if response:
+    print(response.summary())
+else:
+    print("No response")
