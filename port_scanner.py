6d20c6ece89df8925bf566d427a21f615749e4cf
Amir Eskandari <am.eskandari@icloud.com>
2024-12-07 09:56:32 -0800

    Update port_scanner.py

---
+++ port_scanner.py
@@ -0,0 +1,22 @@
+class PortScanner:
+    def __init__(self, target, start_port=1, end_port=65535, delay=0):
+        pass
+
+    def validate_inputs(self):
+        pass
+
+    def scan_port(self, port):
+        pass
+
+    def interpret_response(self, response):
+        pass
+
+    def perform_scan(self):
+        pass
+
+    def output_results(self):
+        pass
+
+
+if __name__ == "__main__":
+    pass
